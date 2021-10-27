6aef0ae09c9b8adda1e98097d363355db116625e
Daniel Riedmüller <daniel.riedmueller@iserv.eu>
2021-10-27 18:37:23 +0200

    removed cors

--- pages/api/match/permissions.js
+++ pages/api/match/permissions.js
@@ -1,6 +1,19 @@
+import Cors from 'cors'
+import initMiddleware from '../../../lib/init-middleware'
 const db = require('better-sqlite3')(process.env.DATABASE_URL);
 
+// Initialize the cors middleware
+const cors = initMiddleware(
+    // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+    Cors({
+        // Only allow requests with GET, POST and OPTIONS
+        methods: ['GET', 'POST', 'OPTIONS'],
+    })
+)
+
 export default async function handle(req, res) {
+    await cors(req, res);
+
     const {id} = req.body;
 
     const permissions = await fetchPermissions(id);
@@ -9,6 +22,7 @@ export default async function handle(req, res) {
 }
 
 export async function fetchPermissions(id) {
+
     let permissions = db
         .prepare('SELECT id, username, is_allowed.allow_match_with as isAllowed, has_given_permission.allow_match_with AS hasGivenPermission ' +
             'FROM user ' +
