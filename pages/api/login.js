38b737f19ae7659215c5755ed2d7cbebeba8f4c0
Daniel Riedmüller <daniel.riedmueller@iserv.eu>
2021-10-27 18:33:33 +0200

    removed cors

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,20 +1,7 @@
 import withSession from "../../lib/session";
-import Cors from 'cors'
-import initMiddleware from '../../lib/init-middleware'
 const db = require('better-sqlite3')(process.env.DATABASE_URL);
 
-// Initialize the cors middleware
-const cors = initMiddleware(
-    // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
-    Cors({
-        // Only allow requests with GET, POST and OPTIONS
-        methods: ['GET', 'POST', 'OPTIONS'],
-    })
-)
-
 export default withSession(async (req, res) => {
-    await cors(req, res);
-
     const {username} = req.body;
 
     let row = db.prepare('SELECT * FROM user WHERE username = ?').get(username);
--- pages/api/match/permissions.js
+++ pages/api/match/permissions.js
@@ -1,19 +1,6 @@
-import Cors from 'cors'
-import initMiddleware from '../../../lib/init-middleware'
 const db = require('better-sqlite3')(process.env.DATABASE_URL);
 
-// Initialize the cors middleware
-const cors = initMiddleware(
-    // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
-    Cors({
-        // Only allow requests with GET, POST and OPTIONS
-        methods: ['GET', 'POST', 'OPTIONS'],
-    })
-)
-
 export default async function handle(req, res) {
-    await cors(req, res);
-
     const {id} = req.body;
 
     const permissions = await fetchPermissions(id);
